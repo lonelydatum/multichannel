dc3696d98d910e2a2f244a0ccd24d211d0d3e6fa
Gar <gar@lonelydatum.com>
2022-01-11 12:01:03 -0500

    ON

--- dev/campaign_multi_300x250/_js/main.js
+++ dev/campaign_multi_300x250/_js/main.js
@@ -9,10 +9,10 @@ const tl = init()
 TweenLite.set(".zero", {x:-150, y:-125})
 
 tl.from(".zero",  {duration:.3, scale:1, ease:'back.out'})
-tl.from(".bring",  {duration:.3, opacity:0, x:"-=100"})
+tl.from(".bring",  {duration:.3, opacity:0, x:"-=100"}, "+=.8")
 
 
-tl.add("chev", "+=.5")
+tl.add("chev", "+=.8")
 tl.to(".zero",  {duration:.3, opacity:0}, "chev")
 tl.from(".chev_1",  {duration:.3, opacity:0}, "chev")
 tl.from(".chev_2",  {duration:.3, opacity:0}, "-=.2")
--- dev/rtb_parlay_single_300x250/_bundled/main.js
+++ dev/rtb_parlay_single_300x250/_bundled/main.js
@@ -54,22 +54,23 @@ module.exports = { olg: olg };
 
 var _commonJsCommonJs = require('../../_common/js/common.js');
 
-TweenLite.set(".bottom_1", { x: 2 * _commonJsCommonJs.size.w });
-TweenLite.set(".bottom_2", { x: _commonJsCommonJs.size.w });
+TweenLite.set(".bottom_1", { x: 0 });
+TweenLite.set(".bottom_2", { x: 1 * _commonJsCommonJs.size.w });
+TweenLite.set(".bottom_3", { x: 2 * _commonJsCommonJs.size.w });
 
-TweenLite.set(".top_1", { x: -_commonJsCommonJs.size.w * 2 });
-TweenLite.set(".top_2", { x: -_commonJsCommonJs.size.w });
+TweenLite.set(".top_1", { x: -0 });
+TweenLite.set(".top_2", { x: -_commonJsCommonJs.size.w * 1 });
+TweenLite.set(".top_3", { x: -_commonJsCommonJs.size.w * 2 });
 
 var tl = (0, _commonJsCommonJs.init)();
 tl.add("dash", 0);
-TweenLite.to(".dash.top", { duration: 14, x: "+=600", ease: "power1.out" }, "dash");
-TweenLite.to(".dash.bottom", { duration: 14, x: "-=600", ease: "power1.out" }, "dash");
+TweenLite.to(".dash.top", { duration: 14, x: "+=" + _commonJsCommonJs.size.w * 2, ease: "power1.out" }, "dash");
+TweenLite.to(".dash.bottom", { duration: 14, x: "-=" + _commonJsCommonJs.size.w * 2, ease: "power1.out" }, "dash");
 
 tl.add("zero", .3);
-tl.from(".zero.left", { duration: .4, x: "-=150" }, "zero");
-tl.from(".zero.right", { duration: .4, x: "+=150" }, "zero");
+tl.from(".zero.left", { duration: .4, x: "-=100" }, "zero");
+tl.from(".zero.right", { duration: .4, x: "+=100" }, "zero");
 tl.from(".proline", { duration: .3, opacity: 0, y: "-=50" }, "+=.1");
-
 tl.to(".proline", { duration: .3, opacity: 0, y: "+=100" }, "+=.9");
 
 tl.add("text");
@@ -77,26 +78,28 @@ tl.from(".t1a", { duration: .3, opacity: 0, y: "-=150" }, "text");
 tl.from(".t1b", { duration: .3, opacity: 0, y: "+=150" }, "text");
 tl.to(".t1", { duration: .2, opacity: 0, y: "+=70" }, "+=3.2");
 
-tl.add("zero_middle", "+=.3");
-tl.from(".zero_middle", { duration: .3, y: "-=250" }, "zero_middle");
+tl.from(".zero_middle", { duration: .3, y: "-=" + _commonJsCommonJs.size.h }, "+=.2");
+
 tl.add("bring", "+=.1");
 tl.from(".green", { duration: .1, opacity: 0 }, "bring");
 tl.from(".bring", { duration: .3, opacity: 0, scale: 1, ease: 'back.out' }, "bring");
 
-tl.add("end", "+=1");
-tl.to([".green", ".zero_middle"], { duration: .3, opacity: 0 }, "end");
-tl.to(".zero", { duration: .3, opacity: 0 }, "end");
+tl.to([".zero", ".green", ".zero_middle"], { duration: .3, opacity: 0 }, "+=1");
 
 tl.to(".bring", { duration: .3, x: -_commonJsCommonJs.size.w / 2, y: -_commonJsCommonJs.size.h / 2, scale: .5 }, "+=.3");
-tl.from(".text_end", { duration: .3, opacity: 0 }, "+=.3");
-tl.to(".text_end", { duration: .3, opacity: 0 }, "+=2");
+tl.from([".text_end", ".proline_small"], { duration: .3, opacity: 0 }, "+=.3");
+
+tl.add("cta-shift", "+=2");
+tl.to(".text_end", { duration: .3, opacity: 0 }, "cta-shift");
+tl.to(".proline_small", { duration: .3, y: -50 }, "cta-shift");
+
 tl.from(".cta", { duration: .5, opacity: 0 });
 
 tl.add("olg", "+=.1");
-tl.from([".footer", ".proline_small"], { duration: .3, opacity: 0 }, "olg");
+tl.from([".footer"], { duration: .3, opacity: 0 }, "olg");
 tl.add((0, _commonJsCommonJs.olg)(), "olg");
 
-// tl.play("bring_end")
+// tl.play("end")
 
 module.exports = {};
 
