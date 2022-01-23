55eac8ecb7b4fb9cb43629d52c3240158b7faa44
Gar <gar@lonelydatum.com>
2022-01-23 10:49:13 -0500

    ON

---
+++ dev/PROLINE_NFLSBLVI_160x600/_js/main.js
@@ -0,0 +1,6 @@
+import {init, olg, size} from '../../_common/js/common.js'
+import {start, chev, bring, end, superbowl} from '../../_common/js/_campaign.js'
+
+
+
+superbowl()
--- dev/_common/js/_campaign.js
+++ dev/_common/js/_campaign.js
@@ -97,8 +97,36 @@ function horizontal(){
 
 }
 
+
+
+function superbowl(){
+	const tl = init()
+	tl.from(".zero",  {duration:.3, scale:1, ease:'back.out'})
+	tl.from(".proline",  {duration:.3, x:"-=100", opacity:0}, "+=1")
+	tl.to(".proline",  {duration:.3, x:"+=100", opacity:0}, "+=1")
+	tl.from(".bring",  {duration:.3, x:"-=100", opacity:0})
+	tl.to([".zero"],  {duration:.3, x:"+=100", opacity:0}, "+=1")
+	tl.add(chev())
+
+	tl.to(".chev_1b",  {duration:.3, opacity:0}, "+=.5")	
+	tl.from(".txt",  {duration:.3, opacity:0})
+	tl.to([".chev_1a", ".txt", ".bring"],  {duration:.3, opacity:0}, "+=2.5")
+	
+
+	
+	tl.from(".end",  {duration:.3, opacity:0}, "+=.2")
+	tl.from(".cta",  {duration:.3, opacity:0}, "+=.5")
+	
+	tl.add("end", "+=.3")
+
+	tl.add(olg(), "end")
+	tl.from(".footer",  {duration:.5, opacity:0}, "end")
+
+}
+
+
 // tl.play("end")
 
 
-module.exports = {start, bring, chev, end, vertical, horizontal};
+module.exports = {start, bring, chev, end, vertical, horizontal, superbowl};
 
